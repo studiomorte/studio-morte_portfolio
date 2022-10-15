164da472af04f1ca1286e0028d008b92aa1829e4
Rachael Moriconi <93764298+studiomorte@users.noreply.github.com>
2022-10-15 19:23:49 +0800

    projects gsap animation COMPLETE

--- studio-morte-portfolio/src/App.js
+++ studio-morte-portfolio/src/App.js
@@ -1,36 +1,16 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import Navbar from './components/Navbar';
-import ProjectLinkAnimation from './components/ProjectLink';
 import './scss/main.scss';
 
+// PROJECT PAGE -------------------------------
 
-
-
-function App( ) {
-
+function App(  ) {
 
   return (
     <>
       <Navbar />
-
       <div className="main-container">
-      <div class="project_page-container">
-
-          <nav className="project_navigation">
-            <ul>
-              <Link to="mywarhammer">
-                <ProjectLinkAnimation />
-              </Link>
-              <Link to="mywarhammer">
-                <ProjectLinkAnimation />
-              </Link>
-
-            </ul>
-          </nav>
-          <div className="project_background-overlay"></div>
-          <div className="project_background-image1"></div>
         <Outlet />
-      </div>
       </div>
     </>
   );
