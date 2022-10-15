1d6b8e3129135fa01e10acd108bc19a552fe277d
Rachael Moriconi <93764298+studiomorte@users.noreply.github.com>
2022-10-15 17:04:18 +0800

    project-link animation working with multiple components

--- studio-morte-portfolio/src/App.js
+++ studio-morte-portfolio/src/App.js
@@ -1,93 +1,29 @@
 import { Outlet, Link } from "react-router-dom"
 import Navbar from './components/Navbar';
+import ProjectLinkAnimation from './components/ProjectLink';
 import './scss/main.scss';
 
-import { useState, useEffect } from 'react';
-import { gsap } from "gsap";
-import { useLayoutEffect, useRef, useCallback, useContext, createContext, useImperativeHandle, forwardRef } from 'react';
-
-/*
-function Rectangle({ children, timeline, index }) {
-  const el = useRef();
-
-  useEffect(() => {
-    timeline.to(el.current, { opacity: 1 }, index * 0.1 );
-  }, [timeline]);
-
-  return <div className="rectangle" ref={el}>{children}</div>
-}
-
-function ProjectLink({ children, timeline, index }) {
-  const el = useRef();
-
-  useEffect(() => {
-    timeline.to(el.current, { fontSize: 90 }, index * 0.1 );
-  }, [ timeline ])
-
-  return <div className="project_link" ref={el}>{children}</div>
-}
-*/
 
 
-function ProjectLink({ children }) {
-  return <li className="project_link">{children}</li>
-}
 
+function App( ) {
 
-function App( index ) {
-
-  const [reversed, setReversed] = useState(true);
-  const app = useRef();
-  const [tl, setTl] = useState(() => gsap.timeline());
-
-  useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      tl.current = gsap.timeline( { defaults: {duration: 0.5 } } )
-
-      .to(".link", {
-        fontSize: 130,
-        ease: "power1"
-      }, index * 1)
-
-      .to(".rectangle", {
-        opacity: 1,
-        ease: "power1"
-      }, index * 1)
-
-      .to(".project_background-image1", {
-        opacity: 1,
-        ease: "power4"
-      }, index * 1);
-
-    }, app);
-    return () => ctx.revert();
-  }, []);
-
-  useEffect(() => {
-    tl.current.reversed(reversed);
-  }, [reversed]);
 
   return (
     <>
       <Navbar />
 
-      <div className="main-container" ref={app}>
+      <div className="main-container">
       <div class="project_page-container">
 
           <nav className="project_navigation">
             <ul>
-              <ProjectLink>
-                  <div className="rectangle"></div>
-                  <Link to="mywarhammer">
-                      <p 
-                      className="link"
-                      onMouseEnter={() => setReversed(!reversed)}
-                      onMouseLeave={() => setReversed(true)}>
-                          My Warhammer
-                      </p>
-                  </Link>
-                  <div className="rectangle"></div>
-              </ProjectLink>
+              <Link to="mywarhammer">
+                <ProjectLinkAnimation />
+              </Link>
+              <Link to="mywarhammer">
+                <ProjectLinkAnimation />
+              </Link>
 
             </ul>
           </nav>
--- studio-morte-portfolio/src/components/ProjectLink.js
+++ studio-morte-portfolio/src/components/ProjectLink.js
@@ -1,22 +1,26 @@
-import { Link } from 'react'
-import { useLayoutEffect, useRef } from 'react';
-import { gsap } from "gsap";
 import { useState, useEffect } from 'react';
+import { gsap } from "gsap";
+import { useLayoutEffect, useRef, useCallback, useContext, createContext, useImperativeHandle, forwardRef } from 'react';
 
-function ProjectLinkAnimation(  ) {
+
+function ProjectLink({ children }) {
+    return <li className="project_link">{children}</li>
+  }
+  
+function ProjectLinkAnimation( index ) {
 
     const [reversed, setReversed] = useState(true);
     const app = useRef();
     const [tl, setTl] = useState(() => gsap.timeline());
-
-    useLayoutEffect((index) => {
+  
+    useLayoutEffect(() => {
       const ctx = gsap.context(() => {
         tl.current = gsap.timeline( { defaults: {duration: 0.5 } } )
   
         .to(".link", {
           fontSize: 130,
           ease: "power1"
-        }, index * 1)
+        }, index * 0)
   
         .to(".rectangle", {
           opacity: 1,
@@ -26,30 +30,30 @@ function ProjectLinkAnimation(  ) {
         .to(".project_background-image1", {
           opacity: 1,
           ease: "power4"
-        }, index * 1);
+        }, index * 0);
   
       }, app);
       return () => ctx.revert();
-    }, []);
+    }, [tl, index]);
   
     useEffect(() => {
       tl.current.reversed(reversed);
-    }, [reversed]);
+    }, [reversed, tl]);
 
     return (
         <>
-            <li ref={app}>
-                <div className="rectangle"></div>
-                <Link to="mywarhammer">
-                    <p 
-                    className="link"
-                    onMouseEnter={() => setReversed(!reversed)}
-                    onMouseLeave={() => setReversed(true)}>
-                        My Warhammer
-                    </p>
-                </Link>
-                <div className="rectangle"></div>
-            </li>
+            <div ref={app}>
+                <ProjectLink>
+                    <div className="rectangle"></div>
+                        <p 
+                        className="link"
+                        onMouseEnter={() => setReversed(!reversed)}
+                        onMouseLeave={() => setReversed(true)}>
+                            My Warhammer
+                        </p>
+                    <div className="rectangle"></div>
+                </ProjectLink>
+            </div>
         </>
     )
 }
