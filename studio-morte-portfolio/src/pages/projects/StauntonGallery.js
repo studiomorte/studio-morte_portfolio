4ad9021d7a32f4cada82327c28efa709bc2286f6
Rachael Moriconi <93764298+studiomorte@users.noreply.github.com>
2022-11-10 00:09:19 +0800

    staunton top section complete

--- studio-morte-portfolio/src/pages/projects/StauntonGallery.js
+++ studio-morte-portfolio/src/pages/projects/StauntonGallery.js
@@ -16,6 +16,8 @@ function Staunton(){
             </header>
             <section className="project-pages_container">
 
+                { /* Opening Section */}
+
                     <div className="opening-block written">
                         <h2>Overview</h2>
                         <p>Staunton Gallery is a local art gallery situated in the heart of Subiaco featuring multiple exhibition spaces. The gallery curates and hosts exhibitions, provides a support network to associated artists, and houses artist studios. Heather Staunton has been operating the gallery as an independent artist since 2007 and is seeking to reimagine the galleries online presence with a brand new website.</p>
@@ -25,7 +27,7 @@ function Staunton(){
                         <span className="fictional-brief">This is a <i><b>fictional brief</b></i>, further details at the end of this page</span>
                     </div>
 
-                    <div className="links-block">
+                    <nav className="links-block">
                         <ul>
                             <li>
                                 <Icon icon="bxl:figma"/>
@@ -48,7 +50,30 @@ function Staunton(){
                                 Codepen
                             </li>
                         </ul>
+                    </nav>
+
+                    <div className="problem-statement">
+                        <p>Staunton Gallery wants to reimagine their online presence with a brand new website that reflects the creative nature of the gallery in a professional, yet aesthetically pleasing manner.</p>
+                        <p>The design must showcase the venue, their exhibitions, artists, and history, while providing information on upcoming events.</p>
                     </div>
+
+                    <div className="project-goals written">
+                        <h2>Project Goals</h2>
+                        <ul>
+                            <li><span>Create an aesthetically pleasing design that leverages white space to convey the 'feeling' that the user is browsing through a physical gallery</span></li>
+                            <li><span>Feature artworks and photography as key structural elements of each page, creating an editoral style design</span></li>
+                            <li><span>Utilise subtle animations to engage users</span></li>
+                            <li><span>Strategically plan page navigation to limit clicks and encourage engagement</span></li>
+                        </ul>
+                    </div>
+
+                    <div className="project-solution written">
+                        <h2>The Solution</h2>
+                        <p>Featuring a soft colour palette, asymmetrical layout, and geometric colour blocks, the final design is a unique and modern website that’s easy to navigate and pleasant to browse. The minimalistic design accomodates the needs of older users, while the subtle animations and clean, editorial style design simultaneously appeals to a younger audience.</p>
+                        <p>The final design is fully responsive, with careful consideration given to layout adjustments made for mobile devices.</p>
+                    </div>
+
+                { /* Case Study Section */ }
 
             </section>
             <Footer />
