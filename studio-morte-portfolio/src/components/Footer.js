fe448c502fbb31135c986d9bafa4b51cd122ce83
Rachael Moriconi <93764298+studiomorte@users.noreply.github.com>
2023-04-19 00:44:34 +0800

    change photo on about page, update about and footer content layout/alignment

--- studio-morte-portfolio/src/components/Footer.js
+++ studio-morte-portfolio/src/components/Footer.js
@@ -1,34 +1,4 @@
-import { Icon } from "@iconify/react";
-
-function SocialIconsFooter() {
-	return (
-		<>
-			<div className="footer__socials-container">
-				<a
-					href="https://www.instagram.com/studiomorte/"
-					className="social_icon"
-				>
-					<Icon icon="ant-design:instagram-filled" width="32px" color="#fff" />
-				</a>
-				<a
-					href="https://www.linkedin.com/in/studiomorte/"
-					className="social_icon"
-				>
-					<Icon icon="brandico:linkedin" width="25px" color="#fff" />
-				</a>
-				<a href="https://medium.com/@studiomorte" className="social_icon">
-					<Icon icon="bi:medium" width="35px" color="#fff" />
-				</a>
-				<a href="https://github.com/studiomorte" className="social_icon">
-					<Icon icon="mingcute:github-fill" width="30px" color="#fff" />
-				</a>
-				<a href="https://codepen.io/studiomorte" className="social_icon">
-					<Icon icon="ph:codepen-logo-fill" width="34px" color="#fff" />
-				</a>
-			</div>
-		</>
-	);
-}
+import SocialIcons from "./SocialIcons";
 
 function Footer() {
 	return (
@@ -36,7 +6,7 @@ function Footer() {
 			<footer className="footer">
 				<div className="footer__container">
 					<p>© 2022 Studio Morte</p>
-					<SocialIconsFooter />
+					<SocialIcons />
 				</div>
 			</footer>
 		</>
--- studio-morte-portfolio/src/components/SocialIcons.js
+++ studio-morte-portfolio/src/components/SocialIcons.js
@@ -1,33 +1,33 @@
 import { Icon } from "@iconify/react";
 
 function SocialIcons() {
-  return (
-    <>
-      <div className="socials__container">
-        <a
-          href="https://www.linkedin.com/in/studiomorte/"
-          className="socials__icon"
-        >
-          <Icon icon="brandico:linkedin" width="30px" />
-        </a>
-        <a href="https://github.com/studiomorte" className="socials__icon">
-          <Icon icon="mingcute:github-fill" width="35px" />
-        </a>
-        <a href="https://codepen.io/studiomorte" className="socials__icon">
-          <Icon icon="ph:codepen-logo-fill" width="39px" />
-        </a>
-        <a
-          href="https://www.instagram.com/studiomorte/"
-          className="socials__icon"
-        >
-          <Icon icon="ant-design:instagram-filled" width="37px" />
-        </a>
-        <a href="https://medium.com/@studiomorte" className="socials__icon">
-          <Icon icon="bi:medium" width="40px" />
-        </a>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="socials__container">
+				<a
+					href="https://www.linkedin.com/in/studiomorte/"
+					className="socials__icon"
+				>
+					<Icon icon="brandico:linkedin" />
+				</a>
+				<a href="https://github.com/studiomorte" className="socials__icon">
+					<Icon icon="mingcute:github-fill" />
+				</a>
+				<a href="https://codepen.io/studiomorte" className="socials__icon">
+					<Icon icon="ph:codepen-logo-fill" />
+				</a>
+				<a
+					href="https://www.instagram.com/studiomorte/"
+					className="socials__icon"
+				>
+					<Icon icon="ant-design:instagram-filled" />
+				</a>
+				<a href="https://medium.com/@studiomorte" className="socials__icon">
+					<Icon icon="bi:medium" />
+				</a>
+			</div>
+		</>
+	);
 }
 
 export default SocialIcons;
